c31ebbe3e6a53afc7247ca9a49a3fc59f215ec14
Mahendra Pratap Das <mahendrapratapdas7@gmail.com>
2024-03-14 11:44:17 +0530

    A-Mahendra|M-Add the jsx syntax with BABEL

--- createDomeElement/index.js
+++ createDomeElement/index.js
@@ -2,5 +2,10 @@
 // heading.textContent="Welcome to JS"
 // document.getElementById("root").appendChild(heading)
 
-const reactHeading = React.createElement("h1",{className:"head"},"Hii React");
-ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+//React with js
+// const reactHeading = React.createElement("h1",{className:"head"},"Hii React");
+// ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+
+//React with jsx
+const jsxHeading = (<h1>Hello jsx</h1>);
+ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
