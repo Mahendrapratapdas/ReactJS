32ca12c701ddbccf21229752eee8e7d2a66b48d9
Mahendra Pratap Das <mahendrapratapdas7@gmail.com>
2024-03-14 15:20:38 +0530

    A-Mahendra|M-Learn functional component in ReactJs with arrow function

--- createDomeElement/index.js
+++ createDomeElement/index.js
@@ -7,5 +7,20 @@
 // ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
 
 //React with jsx
-const jsxHeading = (<h1>Hello jsx</h1>);
-ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
+// const jsxHeading = (<h1>Hello jsx</h1>);
+// ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
+
+//React with component
+function Name(){
+    return(
+        <>
+        <p>My name is Mahendra</p>
+        </>
+    )
+}
+const App = () => (
+        <>
+        <h1>React App with component</h1>
+        </>
+    )
+ReactDOM.createRoot(document.getElementById("root")).render(<><App/><Name/></>)
